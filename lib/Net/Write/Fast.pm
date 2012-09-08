e5b88d53f2d7f12819f7e47f4a653e2daf5b33dd
Patrice Auffret <gomor@cpan.org>
2012-09-08 08:16:21 +0000

    Import of GOMOR/Net-Write-Fast-0.12 from CPAN.
    gitpan-cpan-distribution: Net-Write-Fast
    gitpan-cpan-version:      0.12
    gitpan-cpan-path:         GOMOR/Net-Write-Fast-0.12.tar.gz
    gitpan-cpan-author:       GOMOR
    gitpan-cpan-maturity:     released

--- lib/Net/Write/Fast.pm
+++ lib/Net/Write/Fast.pm
@@ -1,5 +1,5 @@
 #
-# $Id: Fast.pm 12 2012-04-14 12:25:44Z gomor $
+# $Id: Fast.pm 22 2012-09-08 08:14:04Z gomor $
 #
 package Net::Write::Fast;
 use strict;
@@ -7,7 +7,7 @@ use warnings;
 
 use base qw(Exporter DynaLoader);
 
-our $VERSION = '0.11';
+our $VERSION = '0.12';
 
 __PACKAGE__->bootstrap($VERSION);
 
@@ -36,7 +36,53 @@ Net::Write::Fast - create and inject packets fast
 
 =head1 SYNOPSIS
 
+   use Net::Write::Fast;
+
+   # Sends multiple TCP SYNs to the same IPv4 target
+   my $r = Net::Write::Fast::l4_send_tcp_syn(
+      "192.168.0.1",    # IPv4 source
+      "192.168.0.10",   # IPv4 destination
+      [ 25, 80, 110 ],  # TCP port targets
+      200,              # Number of packet per second
+      0,                # Use IPv4
+   );
+
+   # Sends multiple TCP SYNs to multiple IPv4 targets
+   my $r = Net::Write::Fast::l4_send_tcp_syn_multi(
+      "::1",            # IPv6 source
+      [ '::2', '::3' ], # IPv6 targets
+      [ 25, 80, 110 ],  # TCP port targets
+      200,              # Number of packet per second
+      3,                # Number of try
+      1,                # Use IPv6
+   );
+
+   # Handle errors
+   if ($r == 0) {
+      print STDERR "ERROR: ",Net::Write::Fast::nwf_geterror(),"\n";
+   }
+
 =head1 DESCRIPTION
+
+Sends network frames fast to the network.
+
+=head1 METHODS
+
+=over 4
+
+=item B<l4_send_tcp_syn> (ip_src, ip_dst, ports array, ports count, packet per second, useIPv6 flag)
+
+Sends a single TCP SYN at layer 4. Returns 0 in case of failure, and sets error buffer to an error message.
+
+=item B<l4_send_tcp_syn_multi> (ip_src, ip_dst array, ip_dst count, ports array, ports count, packet per second, try count, useIPv6 flag)
+
+Sends multiple TCP SYN at layer 4 to multiple IP targets. Returns 0 in case of failure, and sets error buffer to an error message.
+
+=item B<nwf_geterror>
+
+Get latest error message.
+
+=back
 
 =head1 AUTHOR
 
